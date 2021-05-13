11aa9f95e9d3f17126b046c505f1dc379d71802f
Extacy <git@extacy.cc>
2021-05-13 19:03:26 +1200

    add precompiled header

--- src/il2cpp/framework/helpers.cpp
+++ src/il2cpp/framework/helpers.cpp
@@ -1,33 +1,13 @@
 // Generated C++ file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
 // Helper functions
 
-#include "il2cpp-appdata.h"
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <string>
-#include <codecvt>
+#include "pch.h"
 #include "helpers.h"
-
-// Log file location
-extern const LPCWSTR LOG_FILE;
+#include <codecvt>
 
 // Helper function to get the module base address
 uintptr_t il2cppi_get_base_address() {
     return (uintptr_t) GetModuleHandleW(L"GameAssembly.dll");
-}
-
-// Helper function to append text to a file
-void il2cppi_log_write(std::string text) {
-    HANDLE hfile = CreateFileW(LOG_FILE, FILE_APPEND_DATA, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (hfile == INVALID_HANDLE_VALUE)
-        MessageBoxW(0, L"Could not open log file", 0, 0);
-
-    DWORD written;
-    WriteFile(hfile, text.c_str(), (DWORD) text.length(), &written, NULL);
-    WriteFile(hfile, "\r\n", 2, &written, NULL);
-    CloseHandle(hfile);
 }
 
 // Helper function to open a new console window and redirect stdout there
---
+++ src/pch.h
@@ -0,0 +1,12 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "il2cpp-appdata.h"
+#include "il2cpp-types.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+#endif //PCH_H
